d340af472e49f0d87b38075fb15823a32c76c553
Cameron Ernest <cameron.ernest@outlook.com>
2022-02-08 21:23:46 -0500

    final upload

---
+++ guess the number.py
@@ -0,0 +1,46 @@
+#learnt from Codeacademy
+
+import random
+
+##############################################
+##### You're guessing Computer's number ######
+##############################################
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+            guess=int(input(f"Guess a number between 1 and {x}: "))
+            if(guess < random_number):
+                print("Sorry, guess again. Too low!")
+            elif(guess > random_number):
+                print("Sorry, guess again. Too high!")
+
+    print(f"Yay, you have guessed the number {random_number} correctly!!!")
+
+guess(10)
+
+
+#############################################
+####### Computer guesses your number ########
+#############################################
+
+print()
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low     # could also be high b/c low = high
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)?? ").lower()
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low == guess + 1
+    
+    print(f"Yayyy!! The computer guess your number {guess} correctly!!")
+
+computer_guess(10)
